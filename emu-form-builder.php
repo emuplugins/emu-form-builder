c5887771643c020fb79ac80818e6f5143f60f39e
emuplugins <tonnysantanasantos@gmail.com>
2025-03-24 01:26:03 -0300

    a

--- emu-form-builder.php
+++ emu-form-builder.php
@@ -13,21 +13,21 @@ if (!defined('ABSPATH')) {
 }
 // before header output
 
-define('EFB_PLUGIN_PATH', dirname(__FILE__));
+define('EFB_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('EFB_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 if (is_admin()) {
     require_once EFB_PLUGIN_PATH . '/update-handler.php';
 }
 
-require_once EFB_PLUGIN_PATH . '/inc/classes/EmuAuthenticate.php';
-require_once EFB_PLUGIN_PATH . '/inc/api/login.php';
-require_once EFB_PLUGIN_PATH . '/inc/api/register.php';
-require_once EFB_PLUGIN_PATH . '/inc/api/reset-password.php';
-require_once EFB_PLUGIN_PATH . '/inc/api/send-password-code.php';
-require_once EFB_PLUGIN_PATH . '/inc/api/confirm-code.php';
-require_once(EFB_PLUGIN_PATH . '/inc/functions/post-type.php');
-if(!is_admin()){
-    require_once EFB_PLUGIN_PATH . '/inc/form-templates/login-register.php';
-}
-require_once EFB_PLUGIN_PATH . '/builders/core.php';
+// require_once EFB_PLUGIN_PATH . '/inc/classes/EmuAuthenticate.php';
+// require_once EFB_PLUGIN_PATH . '/inc/api/login.php';
+// require_once EFB_PLUGIN_PATH . '/inc/api/register.php';
+// require_once EFB_PLUGIN_PATH . '/inc/api/reset-password.php';
+// require_once EFB_PLUGIN_PATH . '/inc/api/send-password-code.php';
+// require_once EFB_PLUGIN_PATH . '/inc/api/confirm-code.php';
+// require_once(EFB_PLUGIN_PATH . '/inc/functions/post-type.php');
+// if(!is_admin()){
+//     require_once EFB_PLUGIN_PATH . '/inc/form-templates/login-register.php';
+// }
+// require_once EFB_PLUGIN_PATH . '/builders/core.php';
