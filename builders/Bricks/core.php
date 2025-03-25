fda75689c332ed16edcbc4bdd49b195713630335
emuplugins <tonnysantanasantos@gmail.com>
2025-03-24 22:29:38 -0300

    if class bricks exists

--- builders/Bricks/core.php
+++ builders/Bricks/core.php
@@ -1,5 +1,7 @@
 <?php
 
+if ( ! class_exists('\Bricks\Elements') ) return ;
+
 add_action('init', function() {
     \Bricks\Elements::register_element( EFB_PLUGIN_PATH . '/builders/Bricks/widgets/login-form/widget.php');
 }, 11);
